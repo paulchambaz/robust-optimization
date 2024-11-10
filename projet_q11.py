094c8d4b392b42bd8a76e2b463eeb5a128b7e3f1
zelievdm <zelie@2a01cb09b023fad4ec6d1593515b388f.ipv6.abo.wanadoo.fr>
2024-11-10 18:25:06 +0100

    q1.1

---
+++ projet_q11.py
@@ -0,0 +1,40 @@
+from gurobipy import *
+
+nbvar=10
+
+colonnes = range(nbvar)
+
+s1 = [70,18,16,14,12,10,8,6,4,2]
+s2 = [2,4,6,8,10,12,14,16,18,70]
+
+m= Model("projet_q11")
+x = []
+for i in colonnes:
+	x.append(m.addVar(vtype=GRB.INTEGER, lb=0, name="x%d" % (i+1)))
+m.update()
+obj = LinExpr();
+obj =0
+for j in colonnes:
+	obj += s1[j] * x[j]
+print(obj)
+m.setObjective(obj,GRB.MAXIMIZE)
+
+for i in range(nbvar):
+	m.addConstr(x[i] <= 1, "Contrainte%d")
+m.addConstr(68*x[0]+14*x[1]+10*x[2]+6*x[3]+2*x[4]-2*x[5]-6*x[6]-10*x[7]-14*x[8]-68*x[9] <= -1,  "Contrainte%d")
+
+m.optimize()
+print("")
+print("Solution optimale")
+for j in colonnes:
+	print('x%d'%(j+1), '=', x[j].x)
+print("")
+print('Valeur de la fonction objectif :', m.objVal)
+z1=0
+z2=0
+for i in range(nbvar):
+	z1+=s1[i]*x[i].x
+	z2+=s2[i]*x[i].x
+z=(z1,z2)
+print('Valeur de z :',z)
+
